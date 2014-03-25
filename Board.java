2dbe28f76968343f4089ba33fa69c771f9507e08
Wimdit <nonketh@gmail.com>
2014-03-25 15:02:58 +1100

    Update Board.java

--- Board.java
+++ Board.java
@@ -15,7 +15,7 @@ public class Board {
        hexagon pattern.
      */
 
-	char[][] rows;
+	Hex[][] rows;
 
     // Just to test if board was successfully made
 	public static void main(String[] args) {
@@ -23,21 +23,27 @@ public class Board {
         Board thisBoard = new Board(n);
 		for (int i=0; i<thisBoard.rows.length; i++) {
             for (int j=0; j<thisBoard.rows[i].length; j++) {
-                thisBoard.rows[i][j] = '-';
-                System.out.printf("%c ", thisBoard.rows[i][j]);
+                System.out.printf("%c ", thisBoard.rows[i][j].getColour());
             }
             System.out.println();
         }
+		
+		thisBoard.rows[0][0].setColour('B');
+		System.out.println(thisBoard.rows[1][0].getAdjacent()[1].getColour());
 	}
 
     // Constructor for making a Board with n rows
     // Max length of any row is 2*n-1
 	public Board(int n) {
-		rows = new char[2*n-1][];
-		int i;
+		rows = new Hex[2*n-1][];
+		int i, j;
 		for (i=0; i<n; i++) {
-			rows[i] = new char[n+i];
-            rows[2*n-(i+2)] = new char[n+i];
+			rows[i] = new Hex[n+i];
+            rows[2*n-(i+2)] = new Hex[n+i];
+            for (j=0; j < this.rows[i].length; j++) {
+            	rows[i][j] = new Hex(i, j, '-', this);
+            	rows[2*n-(i+2)][j] = new Hex(i, j, '-', this);
+            }
 		}
 	}
 
@@ -48,6 +54,6 @@ public class Board {
 
     // Method for checking if there is a winner
     public int testWin(Board board) {
-
+		return 0;
     }
 }
